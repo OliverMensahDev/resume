87151b0f2554e440ef18a1f2b786e9ad010ca0eb
Oliver Mensah <olivermensah96@gmail.com>
2017-07-25 19:04:52 +0000

    changed json url

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'http://localhost:3000/resumeData.json',
+      url:'https://omensah.github.io/resume/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
