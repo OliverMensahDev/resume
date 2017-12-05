84a54e513f34fceb7c95725f28a65b64300c4d7c
Oliver Mensah <olivermensah96@gmail.com>
2017-12-05 08:12:19 +0000

    firebase message

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,43 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
+var config = {
+  apiKey: "AIzaSyD54j8WJZ54UjIp3LN_jvLEwJvCOB8DcxQ",
+  authDomain: "omensahgithubresume.firebaseapp.com",
+  databaseURL: "https://omensahgithubresume.firebaseio.com",
+  projectId: "omensahgithubresume",
+  storageBucket: "omensahgithubresume.appspot.com",
+  messagingSenderId: "375687604113"
+};
+firebase.initializeApp(config);
 
 class Contact extends Component {
+  constructor(){
+    super();
+    this.state = {
+      name: '',
+      email:'',
+      subject: '',
+      message:''
+    }
+  }
+  handleMessageSubmit = (event) =>{
+    event.preventDefault();
+    let name = event.target.elements.name.value;
+    let email = event.target.elements.email.value;
+    let subject = event.target.elements.subject.value;
+    let message = event.target.elements.message.value;
+    if(name !="" || email != ""|| subject !="" || message !=""){
+      this.setState({name, email,  subject, message}, 
+        ()=>{
+          firebase.database().ref("messages").push(this.state).then(
+            data=>{
+              alert("You meessage is sent");
+            }
+          ).catch(err=> alert(err));
+        })
+    }
+   
+  }
   render() {
     if(this.props.data){
       var name = this.props.data.name;
@@ -28,27 +65,26 @@ class Contact extends Component {
 
             <div className="eight columns">
 
-               <form action="" method="post" id="contactForm" name="contactForm">
+         <form onSubmit={this.handleMessageSubmit}>
 					<fieldset>
-
-                  <div>
+              <div>
 						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input type="text" value="" size="35" id="contactName" name="contactName" />
+						   <input type="text"  size="35" id="contactName" name="name"  required/>
                   </div>
 
                   <div>
 						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input type="text" value="" size="35" id="contactEmail" name="contactEmail" />
+						   <input type="text"  size="35" id="contactEmail" name="email"  required/>
                   </div>
 
                   <div>
 						   <label htmlFor="contactSubject">Subject</label>
-						   <input type="text" value="" size="35" id="contactSubject" name="contactSubject" />
+						   <input type="text"  size="35" id="contactSubject" name="subject"  required/>
                   </div>
 
                   <div>
                      <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
+                     <textarea cols="50" rows="15" id="contactMessage" name="message" required></textarea>
                   </div>
 
                   <div>
@@ -57,39 +93,39 @@ class Contact extends Component {
                         <img alt="" src="images/loader.gif" />
                      </span>
                   </div>
-
-					</fieldset>
-				   </form>
-
-               <div id="message-warning"> Error boy</div>
-				   <div id="message-success">
-                  <i className="fa fa-check"></i>Your message was sent, thank you!<br />
-				   </div>
-
-            </div>
-
-
-            <aside className="four columns footer-widgets">
-
-               <div className="widget widget_contact">
-
-					   <h4>Address and Phone</h4>
-					   <p className="address">
-						   {name}<br />
-						   {street} <br />
-						   {city}, {state} {zip}<br />
-						   <span>{phone}</span>
-					   </p>
-
-				   </div>
-
-
-            </aside>
-      </div>
-
-   </section>
-    );
-  }
-}
-
-export default Contact;
+                  </fieldset>
+                  </form>
+       
+                 <div id="message-warning"> Error boy</div>
+                  <div id="message-success">
+                         <i className="fa fa-check"></i>Your message was sent, thank you!<br />
+                  </div>
+       
+                   </div>
+       
+       
+                   <aside className="four columns footer-widgets">
+       
+                      <div className="widget widget_contact">
+       
+                    <h4>Address and Phone</h4>
+                    <p className="address">
+                      {name}<br />
+                      {street} <br />
+                      {city}, {state} {zip}<br />
+                      <span>{phone}</span>
+                    </p>
+       
+                  </div>
+       
+       
+                   </aside>
+             </div>
+       
+          </section>
+           );
+         }
+       }
+       
+       export default Contact;
+       
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -20,8 +20,8 @@ class Header extends Component {
             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
             <li><a className="smoothscroll" href="#about">About</a></li>
 	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+            {/* <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li> */}
             <li><a className="smoothscroll" href="#contact">Contact</a></li>
          </ul>
 
@@ -29,8 +29,8 @@ class Header extends Component {
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">Im {name}.</h1>
-            <h3>Im a {city} based <span>{occupation}</span> {description}</h3>
+            <h1 className="responsive-headline">{name}.</h1>
+            <h3>I'm a student as well as <span>{occupation} living in {city}, Ghana. </span> {description}</h3>
             <hr />
             <ul className="social">
               {networks}
